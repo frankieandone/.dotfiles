33d8a9702b42eb01ceda0c39da941ff5496f343a
Frankie Merzadyan <22060147+frankieandone@users.noreply.github.com>
2022-08-15 10:15:28 +0100

    feat `now_as_timestamp`: function

--- utils/datetime_utils.sh
+++ utils/datetime_utils.sh
@@ -1,3 +1,8 @@
 now() {
   date "+%Y-%m-%dT%H-%M-%S";
 }
+
+# https://serverfault.com/questions/151109/how-do-i-get-the-current-unix-time-in-milliseconds-in-bash
+now_as_timestamp() {
+  date +%s%3N
+}
