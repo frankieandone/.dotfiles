6ad82a3c1c70278f336fb527ff47995b8ff8ea9c
frankieandone <22060147+frankieandone@users.noreply.github.com>
2024-06-19 07:42:33 +0100

    New: app/tldr

---
+++ app/tldr/init.sh
@@ -0,0 +1,4 @@
+#tealdear does not support changing config.toml path or symlinks
+rm -v $HOME/Library/Application\ Support/tealdeer/config.toml
+cp -v config.toml $HOME/Library/Application\ Support/tealdeer
+
